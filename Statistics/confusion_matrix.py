a6bb1d99ca72bbb7535a9d32533eb191b37d3510
Seung Wook Kim <CEO@Rloha.io>
2019-02-06 20:07:17 +0900

    Create confusion_matrix.py

---
+++ Statistics/confusion_matrix.py
@@ -0,0 +1,7 @@
+from sklearn.metrics import confusion_matrix  
+from sklearn.metrics import classification_report
+aa = [1, 1, 0, 0, 1, 1, 1]
+bb = [0, 1, 0, 0, 1, 0, 1]
+
+confusion_matrix(aa, bb)
+print(classification_report(aa, bb, target_names=['class 0', 'class 1']))
