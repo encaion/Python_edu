f1422607c12beeed0ae7026db523e7cef2166feb
Seung Wook Kim <CEO@Rloha.io>
2019-02-06 13:54:21 +0900

    Create basic_functions.py

---
+++ Text_handling/basic_functions.py
@@ -0,0 +1,12 @@
+text = "aaBcde"
+text[0]
+text[3:]
+text.upper()
+text.lower()
+text.capitalize()
+text.center(30, "@")
+text.count("a")
+text.count("b")
+text.find("B")
+text.find("k")
+text.index("k")
