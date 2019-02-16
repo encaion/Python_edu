f3c615ac53e8f51244e8838e2f176b3e29162180
Seung Wook Kim <CEO@Rloha.io>
2019-02-16 17:18:07 +0900

    Create aggregate.py

---
+++ Pandas/DataFrame/aggregate.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.DataFrame(np.random.randn(10, 3), columns=['A', 'B', 'C'],
+                  index = pd.date_range('1/1/2020', periods = 10))
+df.iloc[3:7] = np.nan
+
+df.agg(['sum', 'min'])
+
+df.agg({'A' : ['sum', 'min'], 'B' : ['min', 'max']})
