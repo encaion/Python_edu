47765cb44f7c817d09fa00f61f904355f43f132b
Seung Wook Kim <CEO@Rloha.io>
2019-02-18 22:07:50 +0900

    Create create_column_with_regex.py

---
+++ Pandas/DataFrame/create_column_with_regex.py
@@ -0,0 +1,7 @@
+import pandas as pd
+df = pd.read_csv("diamonds.csv")
+df["clarity_num"] = df["clarity"].str.extract("([A-Z])", expand = True)
+df.head()
+
+df_sub = df.loc[df["cut"].str.contains("^P", regex = True), :]
+df_sub["cut"].value_counts()
