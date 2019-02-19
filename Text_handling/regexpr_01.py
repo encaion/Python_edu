99b8aa4fb8c29ad4200dfe7eb635ee591d590bce
Seung Wook Kim <CEO@Rloha.io>
2019-02-19 23:08:37 +0900

    Update regexpr_01.py

--- Text_handling/regexpr_01.py
+++ Text_handling/regexpr_01.py
@@ -6,3 +6,10 @@ match.end()
 
 tt = ["a12312b", "a123b", "asdd"]
 [re.sub("[0-9]", "", s) for s in tt]
+
+s_sub = []
+for s in tt:
+    ss = re.search("a.*?b", s)
+    if ss != None:
+        s_sub.append(ss.group())
+s_sub
