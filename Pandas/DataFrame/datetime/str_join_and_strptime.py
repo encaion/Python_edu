298a35a70315a2258a18a684848fc92a477c3f3e
Seung Wook Kim <CEO@Rloha.io>
2019-02-20 23:08:10 +0900

    Create str_join_and_strptime.py

---
+++ Pandas/DataFrame/datetime/str_join_and_strptime.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from datetime import datetime as dt
+
+df = pd.read_csv("bike_weather.csv")
+df = df.iloc[:10, :4]
+df
+
+df["hour"] = df["hour"].astype(str)
+df.dtypes
+
+# 1
+df["time"] = df[["date", "hour"]].apply(lambda x: ' '.join(x), axis = 1)
+df["time"] = df["time"].apply(lambda x: dt.strptime(x, "%Y-%m-%d %H"))
+
+# 2
+df["time"] = df[["date", "hour"]].apply(lambda x: dt.strptime(' '.join(x), "%Y-%m-%d %H"), axis = 1)
+
+# 3
+df["time"] = df["date"].str.cat(df["hour"], " ")
+
+df["hour"].str.replace("[0-9]", "@")
