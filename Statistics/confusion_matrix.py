3ea9b3acff531b04c75c3fecd30d74c24aa7e9c5
Seung Wook Kim <CEO@Rloha.io>
2019-02-22 12:35:47 +0900

    Update confusion_matrix.py

--- Statistics/confusion_matrix.py
+++ Statistics/confusion_matrix.py
@@ -1,11 +1,13 @@
-from sklearn.metrics import confusion_matrix  
-from sklearn.metrics import classification_report
+import sklearn.metrics as skm
 aa = [1, 1, 0, 0, 1, 1, 1]
 bb = [0, 1, 0, 0, 1, 0, 1]
 
-confusion_matrix(aa, bb)
+skm.confusion_matrix(aa, bb)
+skm.accuracy_score(aa, bb)
+skm.recall_score(aa, bb)
+skm.precision_score(aa, bb)
+
+from sklearn.metrics import classification_report
 print(classification_report(aa, bb, target_names=['class 0', 'class 1']))
 
-accuracy_score(aa, bb)
-precision_score(aa, bb)
-recall_score(bb, bb)
+
