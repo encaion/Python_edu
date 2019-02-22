ac60568dfa00ba3f79fc95653393e0e3b6f33814
Seung Wook Kim <CEO@Rloha.io>
2019-02-22 12:33:55 +0900

    Update confusion_matrix.py

--- Statistics/confusion_matrix.py
+++ Statistics/confusion_matrix.py
@@ -5,3 +5,7 @@ bb = [0, 1, 0, 0, 1, 0, 1]
 
 confusion_matrix(aa, bb)
 print(classification_report(aa, bb, target_names=['class 0', 'class 1']))
+
+accuracy_score(aa, bb)
+precision_score(aa, bb)
+recall_score(bb, bb)
