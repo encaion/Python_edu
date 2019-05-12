bbf4d723f473581ef5b2518c5d69e5fbfb4b4779
Seung Wook Kim <CEO@Rloha.io>
2019-05-13 08:23:58 +0900

    Update crosstab.py

--- Pandas/DataFrame/crosstab.py
+++ Pandas/DataFrame/crosstab.py
@@ -2,7 +2,21 @@ import pandas as pd
 df = pd.read_csv("bike.csv")
 df.iloc[:3,:]
 
+# creation
 pd.crosstab(df["season"], df["holiday"])
 pd.crosstab(df["season"], df["holiday"], margins = True)
+
+pd.crosstab([df["season"], df["workingday"]], df["holiday"])
+
+# proportion
 pd.crosstab(df["season"], df["holiday"]).apply(lambda r: r/r.sum(), axis=0)
 pd.crosstab(df["season"], df["holiday"]).apply(lambda r: r/r.sum(), axis=1)
+
+# indexing
+tab = pd.crosstab(df["season"], df["holiday"])
+tab.iloc[0]
+
+tab.iloc[1]
+tab.iloc[1, :]
+
+tab.iloc[:, 1]
