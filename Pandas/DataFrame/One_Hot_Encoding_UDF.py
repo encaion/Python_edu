d208419c26d894af71116a2331afdb61fe09c8ef
Seung Wook Kim <CEO@Rloha.io>
2019-05-13 08:11:54 +0900

    Create One_Hot_Encoding_UDF.py

---
+++ Pandas/DataFrame/One_Hot_Encoding_UDF.py
@@ -0,0 +1,6 @@
+# can add argument of drop_first
+def one_hot(df, cols):
+    for each in cols:
+        dummies = pd.get_dummies(df[each], prefix=each, drop_first=False)
+        df = pd.concat([df, dummies], axis=1)
+    return df
