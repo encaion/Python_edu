cd1b21f44f50be33d6d6ea6147b8f977f1835365
Seung Wook Kim <encaionkim@gmail.com>
2021-09-17 02:07:13 +0900

    Delete linear_regression_01.py

--- DataMining/linear_regression_01.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import statsmodels.formula.api as smf
-
-df = pd.read_csv("linear_regression_01.csv")
-model = smf.ols('y ~ x', data = df)
-result = model.fit()
-print(result.summary())
