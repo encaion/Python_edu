81ad9556b0da9c68519f4c90756412bbb9cf7dd6
Seung Wook Kim <encaionkim@gmail.com>
2021-10-01 18:55:29 +0900

    Delete naver_top_20_02.py

--- crawling/naver_top_20_02.py
+++
@@ -1,5 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-keywords = BeautifulSoup(requests.get("https://www.naver.com").text, "html.parser").select('[class=ah_k]') 
-[word.text for word in keywords]
