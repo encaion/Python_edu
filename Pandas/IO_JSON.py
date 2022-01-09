d3cee899955fff12d04648eb6128d9518ef1fcc4
Seung Wook Kim <encaionkim@gmail.com>
2022-01-10 07:39:53 +0900

    Update IO_JSON.py

--- Pandas/IO_JSON.py
+++ Pandas/IO_JSON.py
@@ -1,6 +1,6 @@
 import pandas as pd
 df = pd.read_csv("bike.csv")
-df.to_json(r"bike.json", orient = "records")
+df.to_json("bike.json", orient = "records")
 
 df = pd.read_json("bike.json")
 df.head()
